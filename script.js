2e2be2edda88171e5ba356abfc78c1a88dfc2c87
madosy <dasomee@outlook.com>
2022-07-03 16:42:41 -0400

    Use Map for evaluation. Formatted Text

--- script.js
+++ script.js
@@ -24,30 +24,61 @@ function getPlayerInput(){
     }
 }
 
+const rpsMap = new Map()
+//rpsMap.get(playerValue).computerSelection --> 'tie' || 'win' || 'lose'
+rpsMap.set('rock', {
+    rock: 'tie',
+    paper: 'lose',
+    scissor: 'win'
+})
+rpsMap.set('paper', {
+    rock: 'win',
+    paper: 'tie',
+    scissor: 'lose'
+})
+rpsMap.set('scissor', {
+    rock: 'lose',
+    paper: 'win',
+    scissor: 'tie'
+})
+
 function playRound () {
     let playerSelection = getPlayerInput();
     let computerSelection = computerPlay();
-    let outcome = evalWinner(playerSelection,computerSelection);
-    console.log(outcome);   
+    // let outcome = evalWinner(playerSelection,computerSelection);
+    let outcome = rpsMap.get(playerSelection)[computerSelection];
+    let resultText = 
+        (outcome == 'lose')? `You lose! ${capText(computerSelection)} beats ${playerSelection}.` :
+        (outcome == 'win')? `You win! ${capText(playerSelection)} beats ${computerSelection}.` :
+        `It's a tie.`;
+    console.log(resultText);   
 }
 
-function evalWinner(playerSelection,computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "It's a tie!"
-    } else if (playerSelection=="rock") {
-        switch (computerSelection){
-            case "paper": return "You lose! Paper beats Rock."
-            case "scissor": return "You win! Rock beats Scissor."
-        }
-    } else if (playerSelection=="paper") {
-        switch (computerSelection){
-            case "rock": return "You win! Paper beats Rock."
-            case "scissor": return "You lose! Scissor beats Paper."
-        }
-    } else if (playerSelection=="scissor") {
-        switch (computerSelection){
-            case "rock": return "You lose! Rock beats scissor."
-            case "paper": return "You win! Scissor beats Paper."
-        }
-    }
+function capText(text) {
+    firstLetter = text[0].toUpperCase();
+    remainder = text.slice(1);
+    return firstLetter.concat(remainder)
 }
+
+
+
+// function evalWinner(playerSelection,computerSelection) {
+//     if (playerSelection == computerSelection) {
+//         return "It's a tie!"
+//     } else if (playerSelection=="rock") {
+//         switch (computerSelection){
+//             case "paper": return "You lose! Paper beats Rock."
+//             case "scissor": return "You win! Rock beats Scissor."
+//         }
+//     } else if (playerSelection=="paper") {
+//         switch (computerSelection){
+//             case "rock": return "You win! Paper beats Rock."
+//             case "scissor": return "You lose! Scissor beats Paper."
+//         }
+//     } else if (playerSelection=="scissor") {
+//         switch (computerSelection){
+//             case "rock": return "You lose! Rock beats scissor."
+//             case "paper": return "You win! Scissor beats Paper."
+//         }
+//     }
+// }
