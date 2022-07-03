d7bbd56beea1da9d616ac5d42ddd80217c812b65
madosy <dasomee@outlook.com>
2022-07-03 15:52:02 -0400

    Get input from player and evaluate winner vs computer

--- script.js
+++ script.js
@@ -11,3 +11,44 @@ function getRandomInt(max) {
     //get random int with a max value
     return Math.floor( Math.random() * max );
 }
+
+function getPlayerInput(){
+    let playerInput = prompt("What's your move?");
+    let playerInput_lc = playerInput.toLowerCase();
+    switch (playerInput_lc){
+        case "rock":
+        case "scissor":
+        case "paper": return playerInput_lc;
+        default: 
+            alert("'"+playerInput+"'" + " is an invalid move! Try again.");
+    }
+}
+
+function playRound () {
+    let playerSelection = getPlayerInput();
+    let computerSelection = computerPlay();
+    console.log(evalWinner(playerSelection,computerSelection));
+
+    
+}
+
+function evalWinner(playerSelection,computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "It's a tie!"
+    } else if (playerSelection=="rock") {
+        switch (computerSelection){
+            case "paper": return "You lose! Paper beats Rock."
+            case "scissor": return "You win! Rock beats Scissor."
+        }
+    } else if (playerSelection=="paper") {
+        switch (computerSelection){
+            case "rock": return "You win! Paper beats Rock."
+            case "scissor": return "You lose! Scissor beats Paper."
+        }
+    } else if (playerSelection=="scissor") {
+        switch (computerSelection){
+            case "rock": return "You lose! Rock beats scissor."
+            case "paper": return "You win! Scissor beats Paper."
+        }
+    }
+}
